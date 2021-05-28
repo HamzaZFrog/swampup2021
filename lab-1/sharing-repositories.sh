9462c860fce1acfc3119099147b4dd678da9acb5
Hamza Zaoui <Hamzaz@jfrog.com>
2021-05-28 13:58:19 +0200

    jfrog projects fix

--- lab-1/sharing-repositories.sh
+++ lab-1/sharing-repositories.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-# TO DO : managing logging and exceptions
-
-cat ./repo-conf-creation-main.yaml | yq | jq -r '.[]|keys[]' |
+yq r repo-conf-creation-main.yaml -j | jq -r '.[]|keys[]' |
 while read -r repo_id; do
     curl -X PUT https://$JFROG_PLATFORM/access/api/v1/projects/_/share/repositories/$repo_id/su115 -H "accept: application/json" -H "Authorization: Bearer ${token}"
 done
